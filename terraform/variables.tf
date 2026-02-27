b573d48c6b5c743f3b5b77318b4aba8280f1a99d
ea-Mitsuoka <yukihide.mitsuoka@dragon.jp>
2026-02-27 13:17:44 +0900

    Make minor changes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,22 @@
-variable "saas_project_id" { type = string }
-variable "customer_project_id" { type = string }
+variable "saas_project_id" {
+  type = string
+}
+variable "customer_project_id" {
+  type = string
+}
 variable "region" {
   type    = string
   default = "us-central1"
 }
-variable "bq_antipattern_analyzer_url" { type = string }
-variable "slack_webhook_url" { type = string }
-variable "gcs_bucket_name" { type = string }
+variable "bq_antipattern_analyzer_url" {
+  type = string
+}
+variable "slack_webhook_url" {
+  type = string
+}
+variable "gcs_bucket_name" {
+  type = string
+}
 variable "time_range_interval" {
   type    = string
   default = "1 DAY"
@@ -15,9 +25,12 @@ variable "worst_query_limit" {
   type    = string
   default = "2"
 }
-
-variable "scheduler_cron" { default = "0 9 * * *" }
-
-# --- リソース名設定 ---
-variable "service_account_id" { default = "gemini-bq-query-analyzer-sa" }
-variable "bq_dataset_id" { default = "audit_master" }
+variable "scheduler_cron" {
+  default = "0 9 * * *"
+}
+variable "service_account_id" {
+  default = "gemini-bq-query-analyzer-sa"
+}
+variable "bq_dataset_id" {
+  default = "audit_master"
+}
